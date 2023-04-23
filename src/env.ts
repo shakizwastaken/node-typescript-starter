613f64d0ad20581f009a347499e3441d7cb33af9
shakizwastaken <rheyahyathebboss@gmail.com>
2023-04-23 11:43:36 +0100

    env validation

---
+++ src/env.ts
@@ -0,0 +1,26 @@
+import { config as envConfig } from "dotenv";
+
+import joi from "joi";
+
+const envVarsSchema = joi
+  .object({
+    NODE_ENV: joi
+      .string()
+      .allow(["development", "production", "test", "provision"])
+      .default("development")
+      .optional(),
+  })
+  .unknown()
+  .required();
+
+export const config = () => {
+  envConfig();
+
+  const { error, value: envVars } = envVarsSchema.validate(process.env);
+  if (error) {
+    throw new Error(`Config validation error: ${error.message}`);
+  }
+};
+
+export const env = envVarsSchema.validate(process.env).value;
+export default env;
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
+//validates .env file
+import { config, env } from "@env";
+config();
+
+const { NODE_ENV } = env; // validated and typesafe env vars
+console.log(NODE_ENV); //development by default
+
 import hello from "@/hello";
-hello(); //prints hello world
+hello(); //prints "hello world"
